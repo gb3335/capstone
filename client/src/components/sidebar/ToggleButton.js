d580ceb1a12727f5ed5f4dd6bba2634328938838
Krishield <krishieldkyle@gmail.com>
2018-12-20 19:43:46 +0800

    account navbar fixed

---
+++ client/src/components/sidebar/ToggleButton.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+import './ToggleButton.css'
+ 
+class ToggleButton extends Component {
+    render() { 
+        return (
+            
+            <a href="#"className="toggle_button">
+                <i className="fa fa-grip-vertical"></i>
+            </a>
+            
+        );
+    }
+}
+ 
+export default ToggleButton;
