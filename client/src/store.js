d2e0efef6336bf93c3b0e26a2d1b26cb745ea3ef
Krishield <krishieldkyle@gmail.com>
2018-12-26 01:45:54 +0800

    Persistent State

--- client/src/store.js
+++ client/src/store.js
@@ -2,11 +2,35 @@ import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 import rootReducer from './reducers';
 
-const initialState = {}
+
+
+const saveToLocalStorage = (state) => {
+    try{
+        const serializedState = JSON.stringify(state)
+        localStorage.setItem('state', serializedState)
+    }catch(e){
+        console.log(e)
+    }
+}
+
+const loadFromLocalStorage = (state) => {
+    try{
+        const serializedState = localStorage.getItem('state')
+        if(serializedState === null) return undefined
+        return JSON.parse(serializedState)
+    }catch(e){
+        console.log(e)
+        return undefined
+    }
+}
+
+const initialState = loadFromLocalStorage()
 
 const middleware = [thunk]
 
 const store = createStore(rootReducer, initialState, compose(
     applyMiddleware(...middleware), (window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__()) || compose));
+
+store.subscribe(()=> saveToLocalStorage(store.getState()))
 
 export default store
