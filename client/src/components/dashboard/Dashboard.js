6d543b6bc39180a1c7f8f33dd04b9767eec4995a
CarlJstn <carljustinedg@gmail.com>
2019-02-11 10:43:07 +0800

    dashboard recent activity table added (not yet functional)

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -1,11 +1,15 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
 import PropTypes from "prop-types";
-import { changePageTitle } from "../../actions/sidebarActions";
+import { BootstrapTable, TableHeaderColumn } from "react-bootstrap-table";
+
 import "./Dashboard.css";
+
 import Spinner from "../common/Spinner";
 import DoughnutChart from "../common/DoughnutChart";
+
 import { getColleges } from "../../actions/collegeActions";
+import { changePageTitle } from "../../actions/sidebarActions";
 
 class Dashboard extends Component {
   constructor() {
@@ -26,6 +30,164 @@ class Dashboard extends Component {
     let researchData;
     let journalData;
     let dashboardItems;
+    const products = [
+      {
+        title: "College Added",
+        date: "Aug. 23, 1998"
+      },
+      {
+        title: "Research Added",
+        date: "Feb. 12, 1392"
+      },
+      {
+        title: "College Edited",
+        date: "Dec. 34, 1829"
+      },
+      {
+        title: "College Added",
+        date: "Aug. 23, 1998"
+      },
+      {
+        title: "Research Added",
+        date: "Feb. 12, 1392"
+      },
+      {
+        title: "College Edited",
+        date: "Dec. 34, 1829"
+      },
+      {
+        title: "College Added",
+        date: "Aug. 23, 1998"
+      },
+      {
+        title: "Research Added",
+        date: "Feb. 12, 1392"
+      },
+      {
+        title: "College Edited",
+        date: "Dec. 34, 1829"
+      },
+      {
+        title: "College Added",
+        date: "Aug. 23, 1998"
+      },
+      {
+        title: "Research Added",
+        date: "Feb. 12, 1392"
+      },
+      {
+        title: "College Edited",
+        date: "Dec. 34, 1829"
+      },
+      {
+        title: "College Added",
+        date: "Aug. 23, 1998"
+      },
+      {
+        title: "Research Added",
+        date: "Feb. 12, 1392"
+      },
+      {
+        title: "College Edited",
+        date: "Dec. 34, 1829"
+      },
+      {
+        title: "College Added",
+        date: "Aug. 23, 1998"
+      },
+      {
+        title: "Research Added",
+        date: "Feb. 12, 1392"
+      },
+      {
+        title: "College Edited",
+        date: "Dec. 34, 1829"
+      },
+      {
+        title: "College Added",
+        date: "Aug. 23, 1998"
+      },
+      {
+        title: "Research Added",
+        date: "Feb. 12, 1392"
+      },
+      {
+        title: "College Edited",
+        date: "Dec. 34, 1829"
+      },
+      {
+        title: "College Added",
+        date: "Aug. 23, 1998"
+      },
+      {
+        title: "Research Added",
+        date: "Feb. 12, 1392"
+      },
+      {
+        title: "College Edited",
+        date: "Dec. 34, 1829"
+      },
+      {
+        title: "College Added",
+        date: "Aug. 23, 1998"
+      },
+      {
+        title: "Research Added",
+        date: "Feb. 12, 1392"
+      },
+      {
+        title: "College Edited",
+        date: "Dec. 34, 1829"
+      },
+      {
+        title: "College Added",
+        date: "Aug. 23, 1998"
+      },
+      {
+        title: "Research Added",
+        date: "Feb. 12, 1392"
+      },
+      {
+        title: "College Edited",
+        date: "Dec. 34, 1829"
+      },
+      {
+        title: "College Added",
+        date: "Aug. 23, 1998"
+      },
+      {
+        title: "Research Added",
+        date: "Feb. 12, 1392"
+      },
+      {
+        title: "College Edited",
+        date: "Dec. 34, 1829"
+      },
+      {
+        title: "College Added",
+        date: "Aug. 23, 1998"
+      },
+      {
+        title: "Research Added",
+        date: "Feb. 12, 1392"
+      },
+      {
+        title: "College Edited",
+        date: "Dec. 34, 1829"
+      },
+      {
+        title: "College Added",
+        date: "Aug. 23, 1998"
+      },
+      {
+        title: "Research Added",
+        date: "Feb. 12, 1392"
+      },
+      {
+        title: "College Edited",
+        date: "Dec. 34, 1829"
+      }
+    ];
 
     if (colleges === null || loading) {
       dashboardItems = (
@@ -60,11 +222,40 @@ class Dashboard extends Component {
 
       dashboardItems = (
         <div className="row">
-          <div className="col-md-6">
-            <DoughnutChart data={researchData} chartTitle="College Research" />
+          <div className="col-md-4">
+            <div className="card">
+              <div className="card-body">
+                <h5 className="card-title" style={{ textAlign: "center" }}>
+                  Total Researches
+                </h5>
+                <DoughnutChart data={researchData} />
+              </div>
+            </div>
           </div>
-          <div className="col-md-6">
-            <DoughnutChart data={journalData} chartTitle="College Journal" />
+          <div className="col-md-4">
+            <div className="card">
+              <div className="card-body">
+                <h5 className="card-title" style={{ textAlign: "center" }}>
+                  Total Researches
+                </h5>
+                <DoughnutChart data={journalData} />
+              </div>
+            </div>
+          </div>
+          <div className="col-md-4">
+            <div className="card">
+              <div className="card-body">
+                <h5 className="card-title" style={{ textAlign: "center" }}>
+                  Recent Activities
+                </h5>
+                <BootstrapTable data={products} pagination>
+                  <TableHeaderColumn dataField="title" isKey>
+                    Activity
+                  </TableHeaderColumn>
+                  <TableHeaderColumn dataField="date">Time</TableHeaderColumn>
+                </BootstrapTable>
+              </div>
+            </div>
           </div>
         </div>
       );
