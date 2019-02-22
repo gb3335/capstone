25d65a7cbfaa52f5ef308e52c3ae9c8910fbbb25
unknown <krishieldkyle@gmail.com>
2019-02-23 04:44:36 +0800

    deleted plagiarismAlgo folder

--- plagiarismAlgo/binding.js
+++
@@ -1,9 +0,0 @@
-var plagiarism
-
-if (process.env.DEBUG) {
-  plagiarism= require('./build/Debug/plagiarism.node')
-} else {
-  plagiarism= require('./build/Release/plagiarism.node')
-}
-
-module.exports = plagiarism
