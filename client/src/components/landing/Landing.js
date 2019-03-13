d7c90bfba917d7004b23a02b285a8160f74a4c0a
CarlJstn <carljustinedg@gmail.com>
2019-03-13 08:19:15 +0800

    landing (not yet done)

--- client/src/components/landing/Landing.js
+++ client/src/components/landing/Landing.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
+import { Link } from "react-router-dom";
 import PropTypes from "prop-types";
 
 import { getColleges } from "../../actions/collegeActions";
@@ -14,9 +15,28 @@ class Landing extends Component {
 
   render() {
     return (
-      <div className="container-fluid p-2">
-        Landing page goes here
-        <div className="test" />
+      <div className="landing">
+        <div className="dark-overlay landing-inner text-light">
+          <div className="container">
+            <div className="row">
+              <div className="col-md-12 text-center">
+                <h2>
+                  BulSU Plagiarism & Grammar Checker for Research Materials
+                </h2>
+                <img
+                  src="https://s3-ap-southeast-1.amazonaws.com/bulsu-capstone/landingLogo/bulsu_red.png"
+                  alt="bulsu-logo"
+                  style={{ width: "auto", height: "130px" }}
+                />
+                <img
+                  src="https://s3-ap-southeast-1.amazonaws.com/bulsu-capstone/landingLogo/CICT.png"
+                  alt="cict-logo"
+                  style={{ width: "auto", height: "130px" }}
+                />
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
