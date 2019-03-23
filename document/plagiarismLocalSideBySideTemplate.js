9a22280c9910989efffba1bb7f11018341c61da2
unknown <krishieldkyle@gmail.com>
2019-03-24 07:47:31 +0800

    br

--- document/plagiarismLocalSideBySideTemplate.js
+++ document/plagiarismLocalSideBySideTemplate.js
@@ -181,12 +181,17 @@ mark {
             <br />
             <br />
             <br />
+            <br />
+            <br />
             <div className="col-md-8 overviewContent pt-2">
                 <p><b>Similarity Score: </b> ${parseFloat(output[0].SimilarityScore).toFixed(2)}%</p>
                 <p className="pt-3"><b>Source Document: </b> ${output[0].Document.Pattern.Name}</p>
                 <p><b>Target Document: </b> ${output[0].Document.Text.Name}</p>
                 <p><b>Plagiarism Level: </b> ${level}</p>
             </div>
+            <br />
+            <br />
+            <br />
             <br />
             <h4 style="font-size: 10px">Text Checked for Plagiarism: ${output[0].Document.Pattern.Name}</h4>
             <div class="context">
