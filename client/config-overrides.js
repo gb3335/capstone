eaf3007a356ba0ec947c0dc4a89311747b59466c
unknown <krishieldkyle@gmail.com>
2019-03-24 20:39:45 +0800

    test

--- client/config-overrides.js
+++ client/config-overrides.js
@@ -5,8 +5,8 @@ const cspConfigPolicy = {
     'default-src': "*  data: blob: 'unsafe-inline' 'unsafe-eval'",
     'base-uri': "'self'",
     'object-src': "'none'",
-    'script-src': ["'self' 'https://code.jquery.com' 'https://cdnjs.cloudflare.com' 'https://stackpath.bootstrapcdn.com' 'https://cdn.jsdelivr.net' "],
-    'style-src': ["'self' 'https://stackpath.bootstrapcdn.com' 'https://use.fontawesome.com' 'https://fonts.googleapis.com' 'https://npmcdn.com' '//cdn.quilljs.com'"]
+    'script-src': ["* data: blob: 'unsafe-inline' 'unsafe-eval'"],
+    'style-src': ["* data: blob: 'unsafe-inline'"]
 };
 
 function addCspHtmlWebpackPlugin(config) {
