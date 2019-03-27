fb8b42724c088b3a6820da2569333344df44ce9e
CarlJstn <carljustinedg@gmail.com>
2019-03-27 19:28:24 +0800

    website logo

--- client/src/components/footer/Footer.js
+++ client/src/components/footer/Footer.js
@@ -6,7 +6,16 @@ class Footer extends Component {
   render() {
     return (
       <div className="footer">
-          <p>Copyright {(new Date().getFullYear())}</p>
+        <p>
+          {" "}
+          <img
+            src="./images/weblogo.png"
+            alt="website-logo"
+            style={{ width: "30px" }}
+          />{" "}
+          BulSU Plagiarism and Grammar Checker. Copyright{" "}
+          {new Date().getFullYear()}
+        </p>
       </div>
     );
   }
