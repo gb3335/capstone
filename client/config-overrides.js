0cc2ff09d1f00879b8573baea1fd8451ba5f6e2f
unknown <krishieldkyle@gmail.com>
2019-04-06 22:33:25 +0800

    added env, csp

--- client/config-overrides.js
+++ client/config-overrides.js
@@ -4,7 +4,9 @@ const cspHtmlWebpackPlugin = require("csp-html-webpack-plugin");
 const cspConfigPolicy = {
     'default-src': "'self'  data: blob: 'unsafe-inline' 'unsafe-eval' code.jquery.com cdnjs.cloudflare.com stackpath.bootstrapcdn.com stackpath.bootstrapcdn.com use.fontawesome.com fonts.googleapis.com npmcdn.com https://s3-ap-southeast-1.amazonaws.com/bulsu-capstone/ fonts.gstatic.com",
     'base-uri': "'self'",
-    'object-src': "'none'"
+    'object-src': "'none'",
+    'script-src': ["'self' 'unsafe-inline' code.jquery.com cdnjs.cloudflare.com stackpath.bootstrapcdn.com"],
+    'style-src': ["'self' 'unsafe-inline' stackpath.bootstrapcdn.com use.fontawesome.com fonts.googleapis.com npmcdn.com"]
 };
 
 function addCspHtmlWebpackPlugin(config) {
