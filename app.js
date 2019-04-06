c7e6e176bc4e0c389c3b47781c7d92d1634c40da
unknown <krishieldkyle@gmail.com>
2019-04-07 00:26:40 +0800

    test csp

--- app.js
+++ app.js
@@ -30,7 +30,8 @@ mongoose
 // Body Parser Middleware
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json({ limit: "25mb" }));
-app.use(express.static(path.join(__dirname, "client/build"))) // ADDED BY KRISHIELD 3/20/2019
+
+app.use(express.static("client/build")); // ADDED BY KRISHIELD 3/20/2019
 
 //Passport Middleware
 app.use(passport.initialize());
