35d50a423eebcf9ebaf81f1466c030cb5ddfd9ed
unknown <krishieldkyle@gmail.com>
2019-04-06 22:07:10 +0800

    csp

--- client/config-overrides.js
+++ client/config-overrides.js
@@ -2,7 +2,7 @@ const {override} = require('customize-cra');
 const cspHtmlWebpackPlugin = require("csp-html-webpack-plugin");
 
 const cspConfigPolicy = {
-    'default-src': "'self'  data: blob: 'unsafe-inline' 'unsafe-eval' code.jquery.com cdnjs.cloudflare.com stackpath.bootstrapcdn.com stackpath.bootstrapcdn.com use.fontawesome.com fonts.googleapis.com npmcdn.com https://s3-ap-southeast-1.amazonaws.com/bulsu-capstone/",
+    'default-src': "'self'  data: blob: 'unsafe-inline' 'unsafe-eval' code.jquery.com cdnjs.cloudflare.com stackpath.bootstrapcdn.com stackpath.bootstrapcdn.com use.fontawesome.com fonts.googleapis.com npmcdn.com https://s3-ap-southeast-1.amazonaws.com/bulsu-capstone/ fonts.gstatic.com",
     'base-uri': "'self'",
     'object-src': "'none'",
     'script-src': ["'self' 'unsafe-inline' code.jquery.com cdnjs.cloudflare.com stackpath.bootstrapcdn.com"],
