334646dc4fb7d638aa72b8b42902b32ccec96bdf
CarlJstn <carljustinedg@gmail.com>
2019-04-24 14:10:51 +0800

    change import to librarian

--- client/src/components/journals/JournalAction.js
+++ client/src/components/journals/JournalAction.js
@@ -104,8 +104,6 @@ class JournalAction extends Component {
     }
   };
 
-
-
   onGenerateReport = () => {
     if (!this.props.journal.buttonDisable) {
       if (
@@ -214,15 +212,21 @@ class JournalAction extends Component {
           </label>
         </div>
 
-        {this.props.auth.user.userType === "ADMINISTRATOR" ?
+        {this.props.auth.user.userType === "LIBRARIAN" ? (
           <div className="btn-group" role="group" aria-label="Second group">
-            <label to="#" htmlFor="input" className="btn btn-light" style={{ fontSize: "14px" }}>
+            <label
+              to="#"
+              htmlFor="input"
+              className="btn btn-light"
+              style={{ fontSize: "14px" }}
+            >
               <i className="fas fa-file-import text-info mr-1" />
               Import Excel File to Database
-         </label>
+            </label>
           </div>
-          : console.log(false)
-        }
+        ) : (
+          console.log(false)
+        )}
 
         <div hidden>
           <ExcelFieldGroup
@@ -400,13 +404,13 @@ class JournalAction extends Component {
                     className="btn btn-info disabled"
                   />
                 ) : (
-                    <input
-                      type="button"
-                      value="Generate Report"
-                      onClick={this.onGenerateReport}
-                      className="btn btn-info"
-                    />
-                  )}
+                  <input
+                    type="button"
+                    value="Generate Report"
+                    onClick={this.onGenerateReport}
+                    className="btn btn-info"
+                  />
+                )}
               </form>
             </div>
           </div>
